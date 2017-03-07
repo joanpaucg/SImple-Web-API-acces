ab1e6d6881bffd81502f844079060586a4b5b009
Joan Pau <joanpau.castells@hotmail.com>
2017-03-07 18:15:39 +0100

    Download the astronomy page and pint their content

--- simpleweb.py
+++ simpleweb.py
@@ -14,7 +14,8 @@ class WeatherClient(object):
     """docstring for WeatherClient """
     url_base="https://api.wunderground.com/api/"
     url_service={
-    "hourly":"/hourly/q/CA/"
+    "hourly":"/hourly/q/CA/",
+    "astronomy":"/astronomy/q/CA/"
 
     }
     def __init__(self,api_key):
@@ -27,6 +28,14 @@ class WeatherClient(object):
         location + ".json"
         f=requests.get(url)
         return json.loads(f.text)
+    def astronomy(self,location):
+        url=WeatherClient.url_base + self.api_key +\
+        WeatherClient.url_service["astronomy"] +\
+        location + ".json"
+        f=requests.get(url)
+        return json.loads(f.text)
+
+
 
 def print_hourly(hourly):
     hourly_forecast=resultat["hourly_forecast"]
@@ -40,6 +49,30 @@ def print_hourly(hourly):
         (hour["wx"],hour["temp"]["metric"],hour["humidity"],)
 
         print "/**************************************************************/\n"
+def print_astronomy(astronomy):
+    print "Moon phase:"
+    print "  Illuminated: "+str(astronomy["moon_phase"]["percentIlluminated"])+" %"
+    print "  Age of Moon: "+str(astronomy["moon_phase"]["ageOfMoon"])
+    print "  Hemisphere" + str(astronomy["moon_phase"]["hemisphere"])
+    print "  Current Time: %s:%s"%\
+    (astronomy["moon_phase"]["current_time"]["hour"],
+    astronomy["moon_phase"]["current_time"]["minute"])
+    print "  Sun Rise: %s:%s" % (astronomy["moon_phase"]["sunrise"]["hour"]
+    ,astronomy["moon_phase"]["sunrise"]["minute"])
+    print "  Sunset: %s:%s" % (astronomy["moon_phase"]["sunset"]["hour"],
+    astronomy["moon_phase"]["sunset"]["minute"])
+    print "  Moon Rise: %s:%s" % (astronomy["moon_phase"]["moonrise"]["hour"],
+    astronomy["moon_phase"]["moonrise"]["minute"])
+    print "  Moon Set: %s:%s" % (astronomy["moon_phase"]["moonset"]["hour"],
+    astronomy["moon_phase"]["moonset"]["minute"])
+    print "Sun Phase"
+    print "  Sun Rise: %s:%s" % (astronomy["moon_phase"]["sunrise"]["hour"]
+    ,astronomy["sun_phase"]["sunrise"]["minute"])
+    print "  Sunset: %s:%s" % (astronomy["moon_phase"]["sunset"]["hour"],
+    astronomy["sun_phase"]["sunset"]["minute"])
+
+
+
 
 
 
@@ -52,5 +85,8 @@ if __name__=='__main__':
     wc=WeatherClient(api_key)
     resultat=wc.hourly("Lleida")
     print_hourly(resultat)
+    resultat=wc.astronomy("Lleida")
+    print_astronomy(resultat)
+
 
     #print resultat
